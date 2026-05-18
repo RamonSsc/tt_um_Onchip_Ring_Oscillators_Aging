a0ce5b1f739860467b4c7f47474ba277aa1a0f7f
rsarmiento <ramon220031@correo.uis.edu.co>
2026-05-18 02:51:26 +0200

    Check for DRC errors

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_Ro_aging (
+module tt_um_RO_aging (
     input  wire       VGND,
     input  wire       VDPWR,    // 1.8v power supply
 //    input  wire       VAPWR,    // 3.3v power supply
